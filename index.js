c288d20f27a9453437bfe64b1ad99ff8846db992
komaldewnani <komaldewnani@gmail.com>
2021-07-07 01:13:32 +0530

    Modified Files

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const taskContainer = document.querySelector(".task__container");   //always specify dot
-const generateNewCard = (taskData)=>{
-   return  `<div class="col-md-4 id = ${taskData.id}">
+const taskContainer = document.querySelector(".task__container");   
+const generateNewCard = (taskData) => {
+  return `<div class="col-md-4 id = ${taskData.id}">
        <div class="card text-center">
          <div class="card-header d-flex justify-content-end gap-2">
            <button type="button" class="btn btn-outline-success"><i class="fas fa-pen"></i></button>
    <button type="button" class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></button>
          </div>
-         <img   src="${taskData.imageurl}" class = "card-img-top" alt="image"/>
+         <img   src="${taskData.imageurl}" class = "card-img-top" />
          <div class="card-body">
           
            <h5 class="card-title">${taskData.tasktitle}</h5>
            <p class="card-text">${taskData.tasktype}</p>
-           <a href="#" class="btn btn-primary">${taskData.taskdescription}</a>
+           <p class="card-text">${taskData.taskdescription}</p>
          </div>
          <div class="card-footer text-muted">
            <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
@@ -21,37 +21,18 @@ const generateNewCard = (taskData)=>{
 }
 
 const saveChanges = () => {
-    const taskData ={
-        id:`${ Date.now()}`,
-        imageurl : document.getElementById("image__url").value,
-        tasktitle:  document.getElementById("task__title").value    ,
-        tasktype:   document.getElementById("task__type").value    ,
-        taskdescription:document.getElementById("task__description").value,
+  const taskData = {
+    id: `${Date.now()}`,
+    imageurl: document.getElementById("image__url").value,
+    tasktitle: document.getElementById("task__title").value,
+    tasktype: document.getElementById("task__type").value,
+    taskdescription: document.getElementById("task__description").value,
 
-    };
-//     const newCard = ` <div class="col-md-4 id = ${taskData.id}">
-//     <div class="card text-center">
-//       <div class="card-header d-flex justify-content-end gap-2">
-//         <button type="button" class="btn btn-outline-success"><i class="fas fa-pen"></i></button>
-// <button type="button" class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></button>
-//       </div>
-//       <img   src="${taskData.imageurl}" class = "card-img-top" alt="image"/>
-//       <div class="card-body">
-       
-//         <h5 class="card-title">${taskData.tasktitle}</h5>
-//         <p class="card-text">${taskData.tasktype}</p>
-//         <a href="#" class="btn btn-primary">${taskData.taskdescription}</a>
-//       </div>
-//       <div class="card-footer text-muted">
-//         <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
-//       </div>
-//     </div>
-//   </div>`;
-   taskContainer.insertAdjacentHTML("beforeend",generateNewCard(taskData));
-    /*insertAdjacentHTML gives 4 options where to place element 
-    1.beforebegin-> before beginning of container
-    2.afterbegin -> before any element present in container but after its beginning
-    3.beforeend -> before ending of container
-    4. afterend-> after ending of container  */
-    
+  };
+
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+  
+
+
+
 }
